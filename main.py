1f1e921d4440aa35087b65e0af990051bf0fb6db
Jared Weinberger <jared.j.weinberger@gmail.com>
2021-02-16 16:19:29 -0700

    Added bulk methods data_access.py, changed year in MikeRecord to int

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from app.auth import AuthProvider
 from app import create_app
 from app.data_access import MariaDBRecordProvider, TextFileMasterPasswordProvider
 from app.routes import register_routes
